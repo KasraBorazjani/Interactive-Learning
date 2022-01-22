fc738ed234a521cfcadeac6820a113a73bb39234
Kasra Borazjani <Kasra.Borazjani@gmail.com>
2022-01-22 15:09:33 +0330

    adding states to the environment class, implementing q_value calculation in agent class

--- HW3/test.py
+++ HW3/test.py
@@ -1,7 +1,10 @@
 import numpy as np
 
-a = np.array([[1,1], [1,-1], [1,0], [0,1], [0,-1], [0,0], [-1,1], [-1,-1], [-1,0]])
-
-b = np.array([1,1])
-
-print(b in a)
+a = np.random.randint(low=0, high=9, size=(2,9,9))
+states = np.array([[i,j] for i in range(2) for j in range(4)])
+b = np.max(a, axis=0)
+c = np.array(states[2])
+print(states)
+print(b)
+print(c)
+print(b[c[0]][c[1]])
